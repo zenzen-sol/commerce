ad635b3eb3b0dd16e457bb0bfd226edfb6686b83
Sol Irvine <sol@zenzen.io>
2023-12-04 14:57:52 +0900

    feat: Add chinese language styles

--- messages/index.ts
+++ messages/index.ts
@@ -5,8 +5,9 @@ import 'server-only';
 // We also get the default import for cleaner types
 const dictionaries = {
   ja: () => import('./ja.json').then((module) => module.default),
-  en: () => import('./en.json').then((module) => module.default)
+  en: () => import('./en.json').then((module) => module.default),
+  zh: () => import('./zh.json').then((module) => module.default)
 };
 
 export const getDictionary = async (locale: SupportedLocale) =>
-  dictionaries[locale]?.() ?? dictionaries.en();
+  dictionaries[locale]?.() ?? dictionaries.ja();
