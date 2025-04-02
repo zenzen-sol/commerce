40456aacbf73d854fc821919ef3b35af30547976
Sol Irvine <sol@zenzen.io>
2025-04-02 12:12:01 +0900

    wip: Saving

--- next.config.js
+++ next.config.js
@@ -1,32 +1,29 @@
 /** @type {import('next').NextConfig} */
 
-const withNextIntl = require('next-intl/plugin')('./i18n.ts');
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
 module.exports = withNextIntl({
-  eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
-    ignoreDuringBuilds: true
-  },
-  experimental: {
-    ppr: true
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  },
-  async redirects() {
-    return [
-      {
-        source: '/password',
-        destination: '/',
-        permanent: true
-      }
-    ];
-  }
+	eslint: {
+		// Disabling on production builds because we're running checks on PRs via GitHub Actions.
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		formats: ["image/avif", "image/webp"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.shopify.com",
+				pathname: "/s/files/**",
+			},
+		],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/password",
+				destination: "/",
+				permanent: true,
+			},
+		];
+	},
 });
