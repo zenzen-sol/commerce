49de23cee4e9261442dcad21f663735cf90e4239
Sol Irvine <sol@zenzen.io>
2025-04-02 12:05:53 +0900

    wip: Saving

--- app/[locale]/company/page.tsx
+++ app/[locale]/company/page.tsx
@@ -1,52 +1,59 @@
-import Footer from 'components/layout/footer';
-import { SupportedLocale } from 'components/layout/navbar/language-control';
+import Footer from "components/layout/footer";
+import type { SupportedLocale } from "components/layout/navbar/language-control";
 
-import Navbar from 'components/layout/navbar';
-import { getShopifyLocale } from 'lib/locales';
-import { getCart, getProduct } from 'lib/shopify';
-import { Product } from 'lib/shopify/types';
-import { unstable_setRequestLocale } from 'next-intl/server';
-import { cookies } from 'next/headers';
-import { Suspense } from 'react';
-import CompanyDetail from './company-detail';
+import Navbar from "components/layout/navbar";
+import { getShopifyLocale } from "lib/locales";
+import { getCart, getProduct } from "lib/shopify";
+import type { Product } from "lib/shopify/types";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { cookies } from "next/headers";
+import { Suspense } from "react";
+import CompanyDetail from "./company-detail";
 
 const { SITE_NAME } = process.env;
 
 export const metadata = {
-  title: SITE_NAME,
-  description: SITE_NAME,
-  openGraph: {
-    type: 'website'
-  }
+	title: SITE_NAME,
+	description: SITE_NAME,
+	openGraph: {
+		type: "website",
+	},
 };
 
-export default async function Page({ params }: { params: { locale?: SupportedLocale } }) {
-  if (!!params?.locale) {
-    unstable_setRequestLocale(params.locale);
-  }
+export default async function Page({
+	params,
+}: { params: { locale?: SupportedLocale } }) {
+	if (!!params?.locale) {
+		unstable_setRequestLocale(params.locale);
+	}
 
-  const cartId = cookies().get('cartId')?.value;
-  let cart;
+	const cartId = cookies().get("cartId")?.value;
+	let cart;
 
-  if (cartId) {
-    cart = await getCart(cartId);
-  }
+	if (cartId) {
+		cart = await getCart(cartId);
+	}
 
-  const promotedItem: Product | undefined = await getProduct({
-    handle: 'gift-bag-and-postcard-set',
-    language: getShopifyLocale({ locale: params?.locale })
-  });
+	const promotedItem: Product | undefined = await getProduct({
+		handle: "gift-bag-and-postcard-set",
+		language: getShopifyLocale({ locale: params?.locale }),
+	});
 
-  return (
-    <div>
-      <Navbar cart={cart} locale={params?.locale} compact promotedItem={promotedItem} />
-      <Suspense fallback={null}>
-        <div className="pt-12">
-          <CompanyDetail />
-        </div>
-      </Suspense>
+	return (
+		<div>
+			<Navbar
+				cart={cart}
+				locale={params?.locale}
+				compact
+				promotedItem={promotedItem}
+			/>
+			<Suspense fallback={null}>
+				<div className="pt-12">
+					<CompanyDetail />
+				</div>
+			</Suspense>
 
-      <Footer cart={cart} />
-    </div>
-  );
+			<Footer cart={cart} />
+		</div>
+	);
 }
