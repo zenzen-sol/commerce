c87dd6443a8346e133a1a16a1f16c6e8b6f28e51
Sol Irvine <sol@zenzen.io>
2025-05-14 10:44:43 +0900

    Update homepage-products.tsx

--- components/grid/homepage-products.tsx
+++ components/grid/homepage-products.tsx
@@ -1,6 +1,7 @@
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { InlineAddToCart } from "components/cart/inline-add-to-cart";
+import Price from "components/price";
 import { getShopifyLocale } from "lib/locales";
 import { getCollectionProducts } from "lib/shopify";
 import type { Product } from "lib/shopify/types";
@@ -59,6 +60,78 @@ function HomepageProductsItem({
 	) : null;
 }
 
+function HomepagePromoItem({
+	item,
+	priority,
+}: { item: Product; priority?: boolean }) {
+	const size = item?.variants?.[0]?.selectedOptions?.find(
+		(option) => option.name === "Size",
+	);
+	const image = item?.variants?.[0]?.image;
+
+	return image ? (
+		<div className="pt-24">
+			<Link className="group block w-full" href={`/product/${item.handle}`}>
+				<span className="relative block aspect-[16/9] overflow-hidden">
+					<GridTileImage
+						src={image?.url}
+						fill
+						priority={priority}
+						alt={item.title}
+					/>
+				</span>
+			</Link>
+
+			<div className="gap-6 pt-6 md:flex md:flex-row">
+				<div
+					className={clsx(
+						"col-span-1 row-span-1 flex flex-col justify-between space-y-6 md:w-1/2 w-full",
+					)}
+				>
+					<div className="font-multilingual block pb-0">
+						<div className={clsx("@container/label")}>
+							<div className="flex flex-col space-y-2">
+								<h3 className="mr-4 line-clamp-4 grow font-serif text-[28px] leading-tight tracking-wider md:text-[28px]">
+									{item.title as string}
+								</h3>
+								<div className="font-multilingual flex flex-row items-center space-x-2 text-[14px] font-normal">
+									<Price
+										className="flex-none"
+										amount={item.priceRange.maxVariantPrice.amount}
+										currencyCode={item.priceRange.maxVariantPrice.currencyCode}
+										currencyCodeClassName="hidden @[275px]/label:inline"
+									/>
+									{!!size && (
+										<>
+											<div>/</div>
+											<div>{size?.value}</div>
+										</>
+									)}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="w-full space-y-6 pt-6 md:w-1/2 md:pt-0">
+					<InlineAddToCart
+						variants={item.variants}
+						availableForSale={item.availableForSale}
+					/>
+					<div className="font-multilingual block space-y-2 font-extralight">
+						<div>{item?.summary?.value}</div>
+						<div className="inline-flex flex-row items-center space-x-1 opacity-50 transition-opacity duration-150 group-hover:opacity-100">
+							<div>Read more.</div>
+							<div>
+								<ChevronRightIcon width={16} />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	) : null;
+}
+
 export async function HomepageProducts({ lang }: { lang?: string }) {
 	// Collections that start with `hidden-*` are hidden from the search page.
 	const homepageItems = await getCollectionProducts({
@@ -66,23 +139,35 @@ export async function HomepageProducts({ lang }: { lang?: string }) {
 		language: getShopifyLocale({ locale: lang }),
 	});
 
-	// console.debug({ homepageItems });
+	const promotionalItems = await getCollectionProducts({
+		collection: "hidden-homepage-promotional-item",
+		language: getShopifyLocale({ locale: lang }),
+	});
 
 	if (!homepageItems[0] || !homepageItems[1] || !homepageItems[2]) return null;
 
 	const [firstProduct, secondProduct, thirdProduct] = homepageItems;
 
+	const [promotionalProduct] = promotionalItems;
+
 	return (
-		<section
-			className={clsx(
-				"mx-auto grid max-w-screen-xl gap-12 px-4 pb-4 ",
-				"grid-cols-1 md:grid-cols-6",
-				"grid-rows-3 md:grid-rows-1",
+		<section className="space-y-12">
+			<section
+				className={clsx(
+					"mx-auto grid max-w-screen-xl gap-12 px-4 pb-4 ",
+					"grid-cols-1 md:grid-cols-6",
+					"grid-rows-3 md:grid-rows-1",
+				)}
+			>
+				<HomepageProductsItem item={firstProduct} priority={true} />
+				<HomepageProductsItem item={secondProduct} priority={true} />
+				<HomepageProductsItem item={thirdProduct} priority={true} />
+			</section>
+			{promotionalProduct && (
+				<section className={clsx("mx-auto grid max-w-screen-lg px-4 pb-4")}>
+					<HomepagePromoItem item={promotionalProduct} priority={true} />
+				</section>
 			)}
-		>
-			<HomepageProductsItem item={firstProduct} priority={true} />
-			<HomepageProductsItem item={secondProduct} priority={true} />
-			<HomepageProductsItem item={thirdProduct} priority={true} />
 		</section>
 	);
 }
