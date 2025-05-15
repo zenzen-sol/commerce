b6a99860266c0c8c4260a920502e9e22f38b3697
Sol Irvine <sol@zenzen.io>
2025-05-15 16:58:48 +0900

    fix: read more link

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,5 +4,6 @@ import type { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
+	console.log("[Revalidate Route] POST handler invoked.");
 	return revalidate(req);
 }
